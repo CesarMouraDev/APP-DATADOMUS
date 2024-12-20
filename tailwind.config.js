a4db8ef1b441ff4ea901e04e6b132fedce8e6cfd
cesarmouradev <cesarmouradeveloper@gmail.com>
2024-12-19 23:11:56 -0300

    Estruturando

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adiciona os arquivos .js, .jsx, .ts, .tsx no src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
